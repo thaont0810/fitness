44e8638325b085263b894d17c4bcaddf22b59bae
ThaoNt <thaont@rikkeisfot.com>
2018-12-04 17:39:23 +0700

    header

---
+++ root/babel/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+
+  $(window).scroll(function () {
+    if($(window).scrollTop() > 50) {
+      $(".nav-container").addClass('sticky');
+    } else {
+      $(".nav-container").removeClass('sticky');
+    }
+  });
+
+
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    nav: true,
+    items: 1,
+    dots: true,
+    nav: false,
+
+  });
+});
